33baed461552e39b13c08f356280466724d22c2d
zhangyd <18859669020@163.com>
2019-04-26 10:13:17 +0800

    update labels

--- src/Config/config.php
+++ src/Config/config.php
@@ -2,22 +2,31 @@
 
 return [
     'adapter' => env('PROMETHEUS_ADAPTER', 'apc'),
-    
+
     'namespace' => 'app',
-    
+
     'namespace_http' => 'http',
-    
+
     'redis' => [
-        'host'                   => env('PROMETHEUS_REDIS_HOST', '127.0.0.1'),
-        'port'                   => env('PROMETHEUS_REDIS_PORT', 6379),
-        'timeout'                => 0.1,  // in seconds
-        'read_timeout'           => 10, // in seconds
+        'host' => env('PROMETHEUS_REDIS_HOST', '127.0.0.1'),
+        'port' => env('PROMETHEUS_REDIS_PORT', 6379),
+        'timeout' => 0.1,  // in seconds
+        'read_timeout' => 10, // in seconds
         'persistent_connections' => false,
     ],
-    
+
     'push_gateway' => [
         'address' => env('PROMETHEUS_PUSH_GATEWAY_ADDRESS', 'localhost:9091')
     ],
-    
-    'buckets_per_route' => []
+
+    'buckets_per_route' => [],
+
+    'label_keys' => [
+        'app_name',
+        'request_uri',
+        'method',
+        'status_code',
+        'client',
+        'version'
+    ],
 ];
--- src/Middleware/RequestPerRoute.php
+++ src/Middleware/RequestPerRoute.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace GallopYD\PrometheusExporter\Middleware;
 
+use GallopYD\DeviceUtil\DeviceUtil;
 use Illuminate\Http\Request;
 use Closure;
 use Illuminate\Support\Facades\Route;
@@ -38,71 +40,100 @@ class RequestPerRoute
 
         $durationMilliseconds = (microtime(true) - $start) * 1000.0;
 
-        $requestUri = $request->getRequestUri();
-        $method = $request->getMethod();
-        $status = $response->getStatusCode();
+        $labelKeys = config('prometheus_exporter.label_keys');
+        $labelValues = $this->getLabelValue($request, $response, $labelKeys);
 
-        $this->requestCountMetric($requestUri, $method, $status);
-        $this->requestLatencyMetric($requestUri, $method, $status, $durationMilliseconds);
+        $this->requestCountMetric($labelKeys, $labelValues);
+        $this->requestLatencyMetric($labelKeys, $labelValues, $durationMilliseconds);
 
         return $response;
     }
 
     /**
-     * @param string $requestUri
-     * @param string $method
-     * @param int $status
+     * Get Label Value
+     * @param $request
+     * @param $response
+     * @param $labelKeys
+     * @return array
+     */
+    private function getLabelValue($request, $response, $labelKeys)
+    {
+        $labelValues = [];
+        foreach ($labelKeys as $labelKey) {
+            switch ($labelKey) {
+                case 'app_name':
+                    $app_name = config('app.name');
+                    array_push($labelValues, $app_name);
+                    break;
+                case 'request_uri':
+                    $request_uri = $request->route()->uri();
+                    array_push($labelValues, $request_uri);
+                    break;
+                case 'method':
+                    $method = $request->getMethod();
+                    array_push($labelValues, $method);
+                    break;
+                case 'status_code':
+                    $status = $response->getStatusCode();
+                    array_push($labelValues, $status);
+                    break;
+                case 'client':
+                    $user_agent = $request->server('HTTP_USER_AGENT');
+                    $detector = DeviceUtil::getDeviceDetector($user_agent);
+                    $client = $detector->getOs('name');
+                    array_push($labelValues, $client);
+                    break;
+                case 'version':
+                    $user_agent = $request->server('HTTP_USER_AGENT');
+                    $detector = DeviceUtil::getDeviceDetector($user_agent);
+                    $version = $detector->getOs('version');
+                    array_push($labelValues, $version);
+                    break;
+                default:
+                    array_push($labelValues, $request->$labelKey);
+                    break;
+
+            }
+        }
+        return $labelValues;
+    }
+
+    /**
+     * @param array $labelKeys
+     * @param array $labelValues
      * @throws \Prometheus\Exception\MetricsRegistrationException
      */
-    private function requestCountMetric(string $requestUri, string $method, int $status)
+    private function requestCountMetric(array $labelKeys, array $labelValues)
     {
         $this->prometheusExporter->incCounter(
             'requests_total',
             'the number of http requests',
             config('prometheus_exporter.namespace_http'),
-            [
-                'request_uri',
-                'method',
-                'status_code'
-            ],
-            [
-                $requestUri,
-                $method,
-                $status
-            ]
+            $labelKeys,
+            $labelValues
         );
     }
 
     /**
-     * @param string $requestUri
-     * @param string $method
-     * @param int $status
-     * @param int $duration
+     * @param array $labelKeys
+     * @param array $labelValues
      * @throws \Prometheus\Exception\MetricsRegistrationException
      */
-    private function requestLatencyMetric(string $requestUri, string $method, int $status, int $duration)
+    private function requestLatencyMetric(array $labelKeys, array $labelValues, int $duration)
     {
         $bucketsPerRoute = null;
 
-        if ($bucketsPerRouteConfig = config('prometheus-exporter.buckets_per_route')) {
-            $bucketsPerRoute = array_get($bucketsPerRouteConfig, $requestUri);
-        }
+//        if ($bucketsPerRouteConfig = config('prometheus-exporter.buckets_per_route')) {
+//            $bucketsPerRoute = array_get($bucketsPerRouteConfig, $requestUri);
+//        }
 
         $this->prometheusExporter->setHistogram(
             'requests_latency_milliseconds',
             'duration of requests',
             $duration,
             config('prometheus_exporter.namespace_http'),
-            [
-                'request_uri',
-                'method',
-                'status_code'
-            ],
-            [
-                $requestUri,
-                $method,
-                $status
-            ],
+            $labelKeys,
+            $labelValues,
             $bucketsPerRoute
         );
     }
